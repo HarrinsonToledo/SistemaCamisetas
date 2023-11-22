e624a0506861f4be3ea26626779d53de884cf6c5
Gleidy725 <gleidy725@gmail.com>
2023-11-22 01:48:07 -0500

    Tengo sueño

--- Frontend/src/app/components/custom/custom.component.ts
+++ Frontend/src/app/components/custom/custom.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-custom',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
 })
 
 export class CustomComponent {
-
+  selectedStamp: string = 'stamp1';
 }
