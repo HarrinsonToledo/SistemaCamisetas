f73f6e9796ed241dbea9a93135e95cb8ea633e3b
Harrinson Toledo Gonzalez <75257608+HarrinsonToledo@users.noreply.github.com>
2023-11-28 23:31:41 -0500

    ya

--- Frontend/src/app/pago/pago.component.ts
+++ Frontend/src/app/pago/pago.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CarritoContainer } from '../interfaces/CarritoContainer';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import * as Notiflix from 'notiflix';
 
 @Component({
   selector: 'app-pago',
@@ -9,12 +11,28 @@ import { CarritoContainer } from '../interfaces/CarritoContainer';
 })
 export class PagoComponent {
   valorTotal: number = 0;
+  Formulario!: FormGroup;
 
-  constructor(private route: ActivatedRoute, public carrito:CarritoContainer) {}
+  constructor(private route: ActivatedRoute, public carrito:CarritoContainer, private form: FormBuilder) {}
 
-    ngOnInit() {
+  ngOnInit() {
+    this.crearForm()
     this.route.queryParams.subscribe(params => {
       this.valorTotal = params['total'] || 0;
     });
+  }
+
+  crearForm() {
+    this.Formulario = this.form.group({
+      Cedula: ['', Validators.required],
+      Nombres: ['', Validators.required],
+      Apellidos: ['', Validators.required],
+      MedioPago: ['', Validators.required]
+    })
+  }
+
+  pagar() {
+    console.log(this.Formulario.value)
+    Notiflix.Report.success("Factura Guardada", "Medio de Pago: " + this.Formulario.value.MedioPago, "ok");
   }
 }
